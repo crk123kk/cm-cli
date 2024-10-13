543f5d682b7365cf166fd45b443cbe68f571eee5
陈墨 <1973657048@qq.com>
2024-10-13 12:13:26 +0800

    feat: 完善功能

--- src/index.js
+++ src/index.js
@@ -5,12 +5,57 @@ console.log('crk cli 启动成功！');
 // 拿到用户输入的参数 并设置版本号
 import { program } from 'commander';
 import fs from 'fs'
+
+import { fileURLToPath } from "url"
 import path from 'path';
 
-const json = fs.readFileSync(path.resolve('./package.json'), 'utf-8')
+// 获取当前模块的目录路径
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const json = fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8')
 const pkg = JSON.parse(json)
 
 // 配置版本号
 program.version(pkg.version, '-v, --version')
+
+
+import inquirer from 'inquirer'
+import { checkPath, downloadTemp } from './utils.js';
+// 配置命令
+program.command('create <projectName>')
+    // 别名
+    .alias('c')
+    // 描述
+    .description('创建一个项目')
+    // 回调函数，拿到用户输入的参数之后执行的函数
+    .action((proName) => {
+        inquirer.prompt([{
+            type: 'input',// 输入框  其它输入：confirm 确认框 list 选择框 checkbox 多选框
+            name: 'name',// 返回值的 key
+            message: '请输入项目名称',// 描述
+            default: proName,// 默认值
+        },
+        {
+            type: 'confirm',
+            name: 'isTs', // 返回值的key
+            message: '是否使用TypeScript',
+            default: false
+        },
+        ]).then(res => {
+            // res { name: 'crk', isTs: true }
+            if (checkPath(res.name)) {
+                console.log('项目已存在');
+                return
+            }
+            if (res.isTs) {
+                downloadTemp('TS', res.name)
+            } else {
+                downloadTemp('JS', res.name)
+            }
+            console.log('res :>> ', res);
+        })
+    })
+
 
 program.parse(process.argv)
